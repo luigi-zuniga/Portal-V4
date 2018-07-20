00f3ab465e1f31267944bd43aa4b5cf08a37bdf1
luigi-zuniga <luis.alberto.zuniga@outlook.com>
2018-07-19 20:29:07 -0400

    P1.0.0.0 PRL - Maqueta fase 0

---
+++ js/boxes-12060_js_file.js
@@ -0,0 +1,56 @@
+/* Agrega código JS aquí */
+
+$(document).ready(function(){
+  var imgs = $("#fuentes_imagenes a");
+  var imgurl = $(imgs[Math.floor(Math.random() * imgs.length)]).attr("href");
+  $("#images_header").css("background-image","url("+imgurl+")");
+});
+
+
+// JavaScript Document
+	$(function() {
+
+				// rholo carousel
+	    // Using custom configuration
+	    
+        //footer MUMA
+	    $("#banners-footerMUMA").carouFredSel({
+	        items: 3,
+	        direction: "right",
+	        prev: {
+	            button: "#prev6",
+	            key: "left"
+	        },
+	        next: {
+	            button: "#next6",
+	            key: "right"
+	        },
+	        scroll: {
+	            items: 1,
+	            // easing   : "easeOutBounce",
+	            duration: 500,
+	            pauseOnHover: true
+	        },
+	        width: 895
+	    }); 
+
+        //footer otras instituciones.
+	    $("#banners-footer").carouFredSel({
+	        items:3,
+	        direction	: "up",
+			prev		: {	
+			    button		: "#prev6",
+			    key			: "left"
+		    },
+			next		: { 
+			    button		: "#next6",
+			    key			: "right"
+		    },
+			scroll 		: {
+			    items       : 3,
+			    // easing   : "easeOutBounce",
+			    duration    : 500,                        
+			    pauseOnHover: true
+	        }  
+	    }); 
+	});
